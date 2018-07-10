4b6cbfaf5fe2166e90720c3d3620fa757f70fb60
Angela Xiao <angelaxiao@fb.com>
2018-07-10 16:18:14 -0700

    Improved login main screen, replaced actionbar with toolbar

--- app/src/main/java/peapod/angela/parstagram/HomeActivity.java
+++ app/src/main/java/peapod/angela/parstagram/HomeActivity.java
@@ -4,7 +4,16 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
+import peapod.angela.parstagram.model.Post;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -23,5 +32,19 @@ public class HomeActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        ParseQuery.getQuery(Post.class).findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < objects.size(); i++) {
+                        Log.d("HomeActivity", "Post[" + i + "] = " + objects.get(i).getDescription());
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/peapod/angela/parstagram/ParseApp.java
+++ app/src/main/java/peapod/angela/parstagram/ParseApp.java
@@ -3,6 +3,9 @@ package peapod.angela.parstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
+
+import peapod.angela.parstagram.model.Post;
 
 public class ParseApp extends Application {
 
@@ -10,6 +13,7 @@ public class ParseApp extends Application {
     public void onCreate() {
         super.onCreate();
 
+        ParseObject.registerSubclass(Post.class);
         final Parse.Configuration configuration = new Parse.Configuration.Builder(this)
                 .applicationId("angela.peapod")
                 .clientKey("fbu@mpk4summer2018")
