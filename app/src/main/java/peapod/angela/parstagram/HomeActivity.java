c93f41315e551b9e39177a716814de403596c4f3
Angela Xiao <angelaxiao@fb.com>
2018-07-10 17:08:09 -0700

    Implemented post with description and linked username

--- app/src/main/java/peapod/angela/parstagram/HomeActivity.java
+++ app/src/main/java/peapod/angela/parstagram/HomeActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
-import com.parse.ParseQuery;
 
 import java.util.List;
 
@@ -33,12 +32,18 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-        ParseQuery.getQuery(Post.class).findInBackground(new FindCallback<Post>() {
+        final Post.Query postsQuery = new Post.Query();
+        postsQuery.getTop().withUser();
+
+
+        postsQuery.findInBackground(new FindCallback<Post>() {
             @Override
             public void done(List<Post> objects, ParseException e) {
                 if (e == null) {
                     for (int i = 0; i < objects.size(); i++) {
-                        Log.d("HomeActivity", "Post[" + i + "] = " + objects.get(i).getDescription());
+                        Log.d("HomeActivity", "Post[" + i + "] = "
+                                + objects.get(i).getDescription()
+                                + "\nusername = " + objects.get(i).getUser().getUsername());
                     }
                 } else {
                     e.printStackTrace();
